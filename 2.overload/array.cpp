36a1829b5510166b81d6664d37cab3c08d847afd
luckyhyom <bs_khm@naver.com>
2024-10-12 21:06:02 +0900

    N차원 배열 소멸자 함수 메모리 해제 로직 수정 및 namespace 적용
    - Array::operator[]를 클래스 외부로 뺀 이유
      Array는 Int를 사용하기 위해 상단에 Int 클래스명을 명시했다.
      Int도 Array를 사용하고있다. 상단에 Array클래스가 정의되어 있다.
      Array는 Int를 [] 연산자 오버로드에 사용해야한다.
      하지만 Int의 내부 정보까지 필요하다면 단순한 클래스 이름 명시만으로는 Int클래스를 사용할 수 없다.
      클래스 서로 필요로 하는 사이이므로 순서상 밖으로 뺄 수 밖에 없다.

--- 2.overload/array.cpp
+++ 2.overload/array.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+namespace MyArray {    
+class Array;
+class Int;
 class Array {
+    friend Int;
+
     const int dim;
     int* size;
+
 
     // 재귀 호출을 통해 N개의 Address를 초기화
     struct Address {
@@ -11,6 +17,7 @@ class Array {
     };
 
     Address* top; // Address를 가리키는 포인터라는 것을 컴파일러에게 알려준다. 메모리는 할당되지 않음
+
     
     void init_address(Address* current) {
         if(!current) return;
@@ -27,17 +34,46 @@ class Array {
         }
     }
 
+    /*
+        return (종료)가 필요한 경우와 아닌 경우는?
+        init과 delete 함수의 로직이 잘 이해가 안된다. 각 함수 로직의 순서를 바꿀순 없나?
+    */
     void delete_address(Address* current) {
         if(!current) return;
-        if(current->level == dim - 1) {
-            delete[] static_cast<int*>(current->next);
-            return;
-        }
-        for (int i = 0; i < size[current->level]; i++) {
+        /*
+            [2][3][4]
+            
+            1. 두번 돈다.
+            2. 세번 돈다.
+            3. 네번 돈다.
+
+            [for문에서 current->level < dim - 1 역할]
+            - Level 2는 for문 실행하지 않도록 해야함, next를 제거하고 있으므로 Level2에서 Level3을 제거하게 되므로.
+                3차원 배열에서 Level4는 없기때문에.. Level3까지만 제거하면 됨
+            - 재귀 멈추는 역할
+        */
+        for (int i = 0; i < size[current->level] && current->level < dim - 1; i++) {
             delete_address(&static_cast<Address*>(current->next)[i]);
         }
-        delete[]  static_cast<Address*>(current);
+        if(current->level == dim - 1) {
+            delete[] static_cast<int*>(current->next);
+        } else {
+            delete[] static_cast<Address*>(current->next);
+        }
+        std::cout << "Deleting level: " << current->level << std::endl;
     }
+
+    //void delete_address(Address* current) {
+    //    if(!current) return;
+    //    if(current->level == dim - 1) {
+    //        delete[] static_cast<int*>(current->next);
+    //        return;
+    //    }
+    //    for (int i = 0; i < size[current->level]; i++) {
+    //        delete_address(&static_cast<Address*>(current->next)[i]);
+    //    }
+    //    std::cout << "Deleting level: " << current->level << std::endl;
+    //}
 
     void copy_values(Address* dest, Address* source) {
         if(!dest || !source) return;
@@ -86,6 +122,7 @@ class Array {
 
         ~Array() {
             delete_address(top);
+            delete top;
             delete[] size;
         }
 
@@ -100,13 +137,69 @@ class Array {
         void print_all() {
             print_all_(top);
         }
+
+        Int operator[](const int index);
 };
+
+
+class Int {
+    void* data;
+    int level;
+    Array* array;
+
+    public:
+        Int(int index, int level = 0, void* data = NULL, Array* array = NULL): level(level), data(data), array(array) {
+            if(index < 0 || index > array->size[level -1]) {
+                data = NULL;
+                return;
+            };
+            if(level == array->dim) {
+                data = static_cast<int*>(static_cast<Array::Address*>(data)->next) + index;
+            } else {
+                data = static_cast<Array::Address*>(static_cast<Array::Address*>(data)->next) + index;
+            }
+        };
+
+        Int operator[](int index) {
+            /*
+                new 유무의 차이..............!
+                new가 있다면
+                1. Heap에 메모리 할당
+                2. 포인터를 반환
+
+                new가 없다면
+                1. 스택에 메모리 할당
+                2. 넘길 때.. 지역변수는 데이터가 소멸되는데 괜찮나?
+            */
+            return Int(index, level + 1, data, array);
+        }
+
+        operator int() {
+            if(data) return *static_cast<int*>(data);
+            return 0;
+        }
+};
+
+Int Array::operator[](const int index) { 
+    /*
+        new 유무의 차이..............!
+        new가 있다면
+        1. Heap에 메모리 할당
+        2. 포인터를 반환
+
+        new가 없다면
+        1. 스택에 메모리 할당
+        2. 넘길 때.. 지역변수는 데이터가 소멸되는데 괜찮나?
+    */
+    return Int(index, 1, static_cast<void*>(top), this);
+}
+}
 
 int main() {
     int dim = 4;
     int size[] = {2,2,2,4};
-    Array* arr = new Array(dim, size);
-    Array* copy = new Array(*arr);
+    MyArray::Array* arr = new MyArray::Array(dim, size);
+    MyArray::Array* copy = new MyArray::Array(*arr);
     arr->print_all();
     copy->print_all();
     delete arr;
