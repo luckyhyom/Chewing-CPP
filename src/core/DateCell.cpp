b0e2ca7505bd4f86ab0fdb5527f3d5045f47191b
luckyhyom <bs_khm@naver.com>
2024-12-28 21:10:00 +0900

    시간 데이터 자료형 정리
     * # std::tm
     * 1. std::tm의 설계상 항상 연도에서 1900을 빼서 저장한다.
     * 2. 월은 0에서부터 시작한다.
     *
     * # 시간 데이터에 사용되는 자료형
     * 1. 메모리 효율을 위해 time_t 자료형으로 시간 데이터를 저장한다.
     * 2. 사람이 읽기 쉬운 형태로 시간을 표현하기 위해 std::tm 구조체를 사용한다.
     *
     * # stream의 결합 순서와 평가 순서
     * 1. << 연산자 사이의 연산의 평가 순서는 정해져 있지 않다.
     * 2. << 연산자의 결합 순서는 왼쪽에서 오른쪽으로 정해져 있다.

--- src/core/DateCell.cpp
+++ src/core/DateCell.cpp
@@ -6,17 +6,28 @@ int DateCell::to_numeric() {
   return static_cast<int>(*value);
 };
 
+/**
+ * # std::tm
+ * 1. std::tm의 설계상 항상 연도에서 1900을 빼서 저장한다.
+ * 2. 월은 0에서부터 시작한다.
+ * 
+ * # 시간 데이터에 사용되는 자료형
+ * 1. 메모리 효율을 위해 time_t 자료형으로 시간 데이터를 저장한다.
+ * 2. 사람이 읽기 쉬운 형태로 시간을 표현하기 위해 std::tm 구조체를 사용한다.
+ * 
+ * # stream의 결합 순서와 평가 순서
+ * 1. << 연산자 사이의 연산의 평가 순서는 정해져 있지 않다.
+ * 2. << 연산자의 결합 순서는 왼쪽에서 오른쪽으로 정해져 있다.
+ */
 std::string DateCell::stringify() {
   std::ostringstream oss;
   const time_t* value = static_cast<const time_t*>(this->data);
-  // time_t를 다시 날짜로 변환 (현지 시간대 기준)
   std::tm* newTimeStruct = std::localtime(value);
   if (newTimeStruct) {
       oss
         << (newTimeStruct->tm_year + 1900) << "-"
-        << (newTimeStruct->tm_mon + 1 < 10 ? "0" : "") << (newTimeStruct->tm_mon + 1) << "-"
-        << (newTimeStruct->tm_mday < 10 ? "0" : "") << newTimeStruct->tm_mday;
+        << std::setw(2) << std::fill('0') << newTimeStruct->tm_mon + 1  << "-"
+        << std::setw(2) << std::fill('0') << newTimeStruct->tm_mday;
   }
-
-  return oss.str(); // date는.. 포맷은 문자열인데.. 타입은 longint라고하니 어떻게 선언하지
+  return oss.str();
 };
