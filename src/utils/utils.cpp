ea80990f90f00712038937446127536e5274899b
luckyhyom <bs_khm@naver.com>
2025-01-15 20:40:39 +0900

    Table 따라치기

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -116,4 +116,53 @@ Table::~Table() {
   }
   delete[] data_table;
 }
+
+void Table::reg_cell(Cell* c, int row, int col) {
+  if(!(row < max_row_size && col < max_col_size)) return;
+  if (data_table[row][col]) {
+    delete data_table[row][col];
+  }
+  data_table[row][col] = c;
+}
+
+int Table::to_numeric(const string& s) {
+  // Cell 이름으로 받는다.
+  int col = s[0] - 'A';
+  int row = atoi(s.c_str() + 1) - 1;
+
+  if (row < max_row_size && col < max_col_size) {
+    if( data_table[row][col]) {
+      return data_table[row][col]->to_numeric();
+    }
+  }
+}
+int Table::to_numeric(int row, int col) {
+  if (row < max_row_size && col < max_col_size && data_table[row][col]) {
+    return data_table[row][col]->to_numeric();
+  }
+  return 0;
+}
+string Table::stringify(const string& s) {
+  // Cell 이름으로 받는다.
+  int col = s[0] - 'A';
+  int row = atoi(s.c_str() + 1) - 1;
+
+  if(row < max_row_size && col < max_col_size) {
+    if (data_table[row][col]) {
+      return data_table[row][col]->stringify();
+    }
+  }
+  return 0;
+}
+
+string Table::stringify(int row, int col) {
+  if (row < max_row_size && col < max_col_size && data_table[row][col]) {
+    return data_table[row][col]->stringify();
+  }
+  return "";
+}
+std::ostream& operator<<(std::stream& o, Table& table) {
+  o << table.print_table();
+  return o;
+}
 }
