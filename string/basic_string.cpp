5634fbea5e5d1cdabbeb7a7b89aafc481aed6ac5
luckyhyom <bs_khm@naver.com>
2025-04-16 17:43:58 +0900

    basic_string, 비교 알고리즘 수정하기

---
+++ string/basic_string.cpp
@@ -0,0 +1,42 @@
+// 문자열 비교 알고리즘 수정하기
+#include <string>
+#include <iostream>
+
+struct my_char_traits : public std::char_traits<char> {
+    static int get_real_rank(char c) {
+      if (isdigit(c)) {
+        return c + 256;
+      }
+      return c;
+    }
+
+    static bool lt(char __c1, char __c2) {
+      return get_real_rank(__c1) < get_real_rank(__c2);
+    }
+
+    static int compare(const char* s1, const char* s2, size_t n) {
+      while (n-- != 0) {
+        if (get_real_rank(*s1) < get_real_rank(*s2)) {
+          return -1;
+        }
+        if (get_real_rank(*s1) > get_real_rank(*s2)) {
+          return 1;
+        }
+        ++s1;
+        ++s2;
+      }
+      return 0;
+    }
+};
+
+int main() {
+  std::string a = "1a";
+  std::string b = "a1";
+  std::cout << "우선 순위가 낮은 문자열: "  << std::boolalpha << (a < b) << std::endl;
+  std::cout << "우선 순위가 낮은 문자열: "  << std::boolalpha << (b < a) << std::endl;
+
+  std::basic_string<char, my_char_traits> c = "1a";
+  std::basic_string<char, my_char_traits> d = "a1";
+  std::cout << "우선 순위가 낮은 문자열: "  << std::boolalpha << (c < d) << std::endl;
+  std::cout << "우선 순위가 낮은 문자열: "  << std::boolalpha << (d < c) << std::endl;
+}
